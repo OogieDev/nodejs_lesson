2591708a84b6f11256297211bd651b94495e5d66
Rain <you@example.com>
2018-08-12 14:01:46 +0300

    lesson 5-6 events

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-var lodash = require('lodash');
+var EventEmitter = require('events').EventEmitter;
 
-console.log(lodash.sum([4,6]));
+var dispatcher = new EventEmitter();
 
-function wrr(argss){
-    console.log('a');
-}
-lodash.delay(wrr, 1000, 'abs');
-console.log('bbbb');
+dispatcher.on('connect', function (data) {
+    console.log('Connect 1', data);
+});
+
+dispatcher.on('connect', function (data) {
+   console.log('Connect2', data);
+});
+
+dispatcher.on('error', function (err) {
+    console.log(err);
+})
+
+dispatcher.emit('connect', {foo: 1});
+
+dispatcher.emit('error', new Error('Something went wrong!'));
+
+
+
+
+
+// var util = require('util');
+//
+// function Car(){}
+//
+// Car.prototype.logName = function () {
+//     console.log('This name is:', this.name);
+// };
+//
+// function BMW(name) {
+//     this.name = name || 'Unknown bmw model';
+// }
+//
+// BMW.prototype.drive = function(){
+//     console.log('Im driveing!');
+// };
+//
+// util.inherits(BMW, Car);
+//
+// var mbw = new BMW('X6');
+// mbw.logName();
