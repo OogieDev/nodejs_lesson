ac5bd54aa61268578409d5bb15fb2db31b0eb0e9
Rain <you@example.com>
2018-08-12 15:24:10 +0300

    lesson9. Node server. part2 easy route

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var http = require('http');
-
+var url = require('url');
+var fs = require('fs');
 
 var server = new http.Server;
+
 
 server.listen(80, '127.0.0.1');
 
 var counter = 0;
 
+
+
 server.on('request', function (req, res) {
-    res.end('hello server' + ++counter);
+    var parseUrl = url.parse(req.url, true);
+
+    fs.readFile(getPageNameByPath(parseUrl.pathname) + '.html', function (err, data) {
+        if(err) throw new Error(err);
+
+        res.end(data);
+    });
 });
+
+function getPageNameByPath(path) {
+    switch (path){
+        case '/':
+        case '/home':
+            return 'index';
+        case '/about':
+            return 'about';
+        default: return '404';
+    }
+}
